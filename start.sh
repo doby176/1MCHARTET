eae16e18d5dc2c444629701b0809542f3e0e6d1a
doby176 <tartogaaa@gmail.com>
2025-07-05 22:44:46 +0300

    Move start.sh to project root and update to use gunicorn#1

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
-# C:\Users\ASUS\1mchartet\1mland\start.sh
 #!/bin/bash
 mkdir -p /opt/render/project/src/data
 touch /opt/render/project/src/data/users.db
 chmod 600 /opt/render/project/src/data/users.db
-python app.py
+gunicorn --bind 0.0.0.0:$PORT app:app
